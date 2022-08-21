53143736e86850460c04cedb7c2cc937be96a7d5
JamelBoyer <jamel-boyer@outlook.com>
2022-08-21 07:07:47 -0400

    adding homework assignment

--- 18-Blockchain/Instructions/Starter_Code/pychain.py
+++ 18-Blockchain/Instructions/Starter_Code/pychain.py
@@ -49,7 +49,15 @@ import hashlib
 # @TODO
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
-# YOUR CODE HERE
+
+@dataclass
+class Record:
+    sender: str
+    receiver: str
+    amount: float
+
+
+
 
 
 ################################################################################
@@ -68,7 +76,8 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
+
+    record: Record
 
     creator_id: int
     prev_hash: str = "0"
@@ -170,15 +179,17 @@ input_data = st.text_input("Block Data")
 
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
+sender = st.text_input("Input Sender Information")
+
 
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
+receiver = st.text_input("Input Receiver Information")
+
 
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
+amount = st.text_input("Input Amount Information")
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
